85792eb2f1a6f46e8e5fd38e396cd9a8eb99d5be
Elg <klink100@gmail.com>
2021-03-02 13:34:13 +0100

    Add proof of concept login

--- src/main/kotlin/no/hvl/dat251/fjelltur/controller/AccountController.kt
+++ src/main/kotlin/no/hvl/dat251/fjelltur/controller/AccountController.kt
@@ -8,11 +8,12 @@ import no.hvl.dat251.fjelltur.dto.RegisteredAccountResponse
 import no.hvl.dat251.fjelltur.dto.toResponse
 import no.hvl.dat251.fjelltur.service.AccountService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
@@ -24,8 +25,15 @@ import javax.validation.Valid
 class AccountController(@Autowired val accountService: AccountService) {
 
   @GetMapping("/me")
-  fun getMe(@RequestParam(value = "name", defaultValue = "World") name: String): String {
-    return "Hello my old friend, $name !"
+  fun getMe(): RegisteredAccountResponse {
+
+    val obj = SecurityContextHolder.getContext().authentication.principal
+
+    if (obj is UserDetails) {
+      return accountService.getAccountByUsername(obj.username).toResponse()
+    }
+
+    throw error("principal is not UserDetails. but ${obj.javaClass}")
   }
 
   @PostMapping("/$REGISTER_PATH")
