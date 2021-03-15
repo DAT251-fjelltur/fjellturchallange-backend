5a479423949b9ff59291e59e25c5ad06d4dde418
Elg <klink100@gmail.com>
2021-03-15 12:52:42 +0100

    Add swagger documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,12 +27,17 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-amqp")
   implementation("org.springframework.boot:spring-boot-starter-web")
-  
+
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.postgresql:postgresql:42.2.18")
   implementation("com.auth0:java-jwt:3.13.0")
+
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.5")
+  implementation("org.springdoc:springdoc-openapi-security:1.5.5")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.5")
+
 
   runtimeOnly("org.springframework.boot:spring-boot-devtools")
 
