57a4d4fb0e2f9529b3b07992bcd0a7da4527ba19
Mathias <mathias.skallerud@gmail.com>
2021-03-23 16:12:13 +0100

    Add methos to find all rules given a name

--- src/main/kotlin/no/hvl/dat251/fjelltur/repository/RuleRepository.kt
+++ src/main/kotlin/no/hvl/dat251/fjelltur/repository/RuleRepository.kt
@@ -3,4 +3,6 @@ package no.hvl.dat251.fjelltur.repository
 import no.hvl.dat251.fjelltur.model.Rule
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface RuleRepository : JpaRepository<Rule, String>
+interface RuleRepository : JpaRepository<Rule, String> {
+  fun findAllByName(name: String): Rule?
+}
