699ff19b76e396dfe6997bfb981bac3ac96e7ffc
Mathias <mathias.skallerud@gmail.com>
2021-03-23 16:19:08 +0100

    fix merge conflicts

--- src/main/kotlin/no/hvl/dat251/fjelltur/service/RuleService.kt
+++ src/main/kotlin/no/hvl/dat251/fjelltur/service/RuleService.kt
@@ -2,10 +2,7 @@ package no.hvl.dat251.fjelltur.service
 
 import no.hvl.dat251.fjelltur.dto.MakeRuleRequest
 import no.hvl.dat251.fjelltur.dto.MakeTimeRuleRequest
-<<<<<<< HEAD
 import no.hvl.dat251.fjelltur.exception.NotUniqueRuleException
-=======
->>>>>>> 8cc1a93f19208f5a3610130adee7ce6ee7c5edb0
 import no.hvl.dat251.fjelltur.model.Rule
 import no.hvl.dat251.fjelltur.model.TimeRule
 import org.springframework.data.domain.Page
